19a7553ffa904006bc2848ad4af9e2b7af47391b
Rubén <Rubn@LAPTOP-8HUSBV2A.lan>
2019-04-09 11:30:09 -0500

    Solución Thread 1

---
+++ Objetos-1902/src/threadEntregable/CerrarVentanaException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threadEntregable;
+
+/**
+ *
+ * @author Rubén
+ */
+public class CerrarVentanaException extends Exception{
+    public CerrarVentanaException()
+    {
+        super("¡Se cerró la ventana!");
+    }
+}
